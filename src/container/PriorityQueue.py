30ef311f7c19bbd0c4746845afd7af1885fb96d8
Andrei Lucaci <lucaciandrei@outlook.com>
2017-03-19 20:11:38 +0200

    Added tests to priority queue

--- src/container/PriorityQueue.py
+++ src/container/PriorityQueue.py
@@ -1,23 +1,29 @@
+from itertools import chain
+
 class PriorityQueue:
     def __init__(self):
-        self.items = {}
-        self.max_priority = 0
+        self._items = {}
+        self._max_priority = 0
 
     def _enqueue_priority(self, item, priority):
-        priority = self.max_priority if not priority else priority
-        if priority not in self.items:
-            self.items[priority] = []
-        self.items[priority].insert(0, item)
-        self.max_priority = self.max_priority if self.max_priority >= priority else priority
+        priority = self._max_priority if not priority else priority
+        if priority not in self._items:
+            self._items[priority] = []
+        self._items[priority].insert(0, item)
+        self._max_priority = self._max_priority if self._max_priority >= priority else priority
         return item
 
-    def _dequeue_priority(self):
-        if not self.items:
+    def _dequeue_priority(self, is_peek=False):
+        if not self._items:
             return None
-        item = self.items[self.max_priority].pop()
-        if not self.items[self.max_priority]:
-            del self.items[self.max_priority]
-            self.max_priority = max(self.items.keys()) if self.items else 0
+
+        if is_peek:
+            return self._items[self._max_priority][-1]
+
+        item = self._items[self._max_priority].pop()
+        if not self._items[self._max_priority]:
+            del self._items[self._max_priority]
+            self._max_priority = max(self._items.keys()) if self._items else 0
         return item
 
     def enqueue(self, item, priority=None):
@@ -27,9 +33,17 @@ class PriorityQueue:
         return self._dequeue_priority()
 
     def size(self):
-        return sum([len(self.items[i]) for i in self.items])
+        return sum([len(self._items[i]) for i in self._items])
 
     def is_empty(self):
-        return self.items == {}
+        return self._items == {}
 
+    def max_priority(self):
+        return self._max_priority
+
+    def peak_all_items(self):
+        return chain(self._items[i] for i in self._items)
+
+    def peek(self):
+        return self._dequeue_priority(True)
 
--- src/tests/test_priority_queue.py
+++ src/tests/test_priority_queue.py
@@ -1,15 +1,48 @@
 from unittest import TestCase
 
+from src.container.PriorityQueue import PriorityQueue
 
 class TestPriorityQueue(TestCase):
-    def test_enqueue(self):
-        self.fail()
+    def create_sut(self):
+        return PriorityQueue()
 
-    def test_dequeue(self):
-        self.fail()
+    def test_enqueue_item_is_added(self):
+        sut = self.create_sut()
+        sut.enqueue(1)
+        self.assertEqual(1, sut.size())
 
-    def test_size(self):
-        self.fail()
+    def test_enqueue_item_with_priority_is_added(self):
+        sut = self.create_sut()
+        sut.enqueue(1, 1)
+        self.assertEqual(1, sut.max_priority())
+        self.assertEqual(1, sut.size())
+
+    def test_dequeue_by_priority(self):
+        sut = self.create_sut()
+        sut.enqueue(1)
+        sut.enqueue(2, 3)
+        sut.enqueue(3, 3)
+
+        self.assertEqual(2, sut.peek())
+        self.assertEqual(3, sut.max_priority())
+
+    def test_dequeue_max_priotity_changes(self):
+        sut = self.create_sut()
+        sut.enqueue(1)
+        sut.enqueue(2, 2)
+        sut.enqueue(3, 3)
+
+        self.assertEqual(3, sut.max_priority())
+        self.assertEqual(3, sut.dequeue())
+        self.assertEqual(2, sut.max_priority())
+
+    def test_dequeue_single_element_the_queue_remains_empty(self):
+        sut = self.create_sut()
+        sut.enqueue(1)
+
+        self.assertEqual(1, sut.dequeue())
+        self.assertTrue(sut.is_empty())
 
     def test_is_empty(self):
-        self.fail()
+        sut = self.create_sut()
+        self.assertTrue(sut.is_empty())
